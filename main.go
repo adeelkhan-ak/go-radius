a68d8a53509ab1a93c337731be3dbdbbbb8e9a9d
adeelkhan-ak <ak140175@gmail.com>
2021-12-23 14:59:19 +0500

    useless comments removed

--- main.go
+++ main.go
@@ -25,56 +25,43 @@ var (
 )
 
 func printPacketInfo(packet gopacket.Packet) {
-	// Let's see if the packet is an ethernet packet
+
+	// Layer2
 	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
 	if ethernetLayer != nil {
 		fmt.Println("Ethernet layer detected.")
 		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
 		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
 		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
-		// Ethernet type is typically IPv4 but could be ARP or other
 		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
 		fmt.Println()
 	}
 
-	// Let's see if the packet is IP (even though the ether type told us)
+	// Layer 3
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer != nil {
 		fmt.Println("IPv4 layer detected.")
 		ip, _ := ipLayer.(*layers.IPv4)
-
-		// IP layer variables:
-		// Version (Either 4 or 6)
-		// IHL (IP Header Length in 32-bit words)
-		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-		// Checksum, SrcIP, DstIP
 		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
 		fmt.Println("Protocol: ", ip.Protocol)
 		fmt.Println()
 	}
 
-	// Let's see if the packet is TCP
+	// Layer 4
 	tcpLayer := packet.Layer(layers.LayerTypeTCP)
 	if tcpLayer != nil {
 		fmt.Println("TCP layer detected.")
 		tcp, _ := tcpLayer.(*layers.TCP)
-
-		// TCP layer variables:
-		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
-		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
 		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
 		fmt.Println("Sequence number: ", tcp.Seq)
 		fmt.Println()
 	}
-
 	// Iterate over all layers, printing out each layer type
 	fmt.Println("All packet layers:")
 	for _, layer := range packet.Layers() {
 		fmt.Println("- ", layer.LayerType())
 	}
 
-	// When iterating through packet.Layers() above,
-	// if it lists Payload layer then that is the same as
 	// this applicationLayer. applicationLayer contains the payload
 	applicationLayer := packet.ApplicationLayer()
 	if applicationLayer != nil {
@@ -159,7 +146,7 @@ func main() {
 	for packet := range packetSource.Packets() {
 		// Do something with a packet here.
 		printPacketInfo(packet)
-		//return
+		return
 	}
 
 }
